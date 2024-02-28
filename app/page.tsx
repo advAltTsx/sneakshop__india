182301e04b7ba53d23e85ae44e31a67df124aa23
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-02-28 17:38:53 +0530

    *

---
+++ app/page.tsx
@@ -0,0 +1,80 @@
+import NavBar from "@/components/Navbar";
+import Image from "next/image";
+import { TypewriterEffect } from "@/components/ui/typewriter-effect";
+import { Spotlight } from "@/components/ui/Spotlight";
+import { TextGenerateEffect } from "@/components/ui/text-generate-effect";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer"
+
+export default function Home() {
+  const tagline = `Looking for the perfect blend of fashion and functionality? Look no further than our premium sneaker shop, where you'll discover an unparalleled range of stylish and comfortable footwear to suit your every need. From sporty and casual to sleek and trendy, our collection is designed to elevate your everyday style and keep you on the move in total comfort.
+`;
+  const words = [
+    {
+      text: "Kick",
+    },
+    {
+      text: "with",
+    },
+    {
+      text: "the",
+    },
+    {
+      text: "Lowkey",
+    },
+    {
+      text: "Rizz.",
+      className: "text-blue-500 dark:text-blue-500",
+    },
+  ];
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between">
+
+      <div className="min-h-screen h-full w-full dark:bg-black bg-white  dark:bg-grid-white/[0.2] bg-grid-black/[0.2] relative flex items-center justify-center">
+        {/* Radial gradient for the container to give a faded look */}
+        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+        <p className="text-4xl sm:text-7xl font-bold relative z-20 bg-clip-text text-transparent bg-gradient-to-b from-neutral-200 to-neutral-500 py-8">
+          <div className="h-auto py-24 px-6 w-full rounded-xl flex md:items-center md:justify-center bg-black/[0.96] antialiased bg-grid-white/[0.02] relative overflow-hidden">
+            <Spotlight
+              className="-top-40 left-0 md:left-60 md:-top-20"
+              fill="white"
+            />
+            <div className="mx-auto relative z-10  w-full md:pt-0">
+              <h1 className="text-4xl md:text-7xl font-bold text-center bg-clip-text text-transparent bg-gradient-to-b from-neutral-50 to-neutral-400 bg-opacity-50">
+                <TypewriterEffect words={words} />
+              </h1>
+              <p className="mt-4 font-normal text-base text-neutral-300 max-w-lg text-center mx-auto">
+                Visit our store today and experience the ultimate in sneaker
+                fashion. Your feet deserve the best - and we're here to make
+                sure you step out in style, every time. Elevate your look.
+                Elevate your comfort. Elevate your game - only at <span className="text-blue-500 font-bold dark:text-blue-500"> Sneak Shop
+                India</span>.
+              </p>
+              <div className="w-full flex items-center justify-center pt-8">
+             
+<Drawer >
+  <DrawerTrigger> <button className="px-8 py-0.5  border-2 border-black dark:border-white uppercase bg-blue-500 text-white transition duration-200 text-xs shadow-[1px_1px_rgba(0,0,0),2px_2px_rgba(0,0,0),3px_3px_rgba(0,0,0),4px_4px_rgba(0,0,0),5px_5px_0px_0px_rgba(0,0,0)] dark:shadow-[1px_1px_rgba(255,255,255),2px_2px_rgba(255,255,255),3px_3px_rgba(255,255,255),4px_4px_rgba(255,255,255),5px_5px_0px_0px_rgba(255,255,255)] ">
+  Read more
+</button></DrawerTrigger>
+  <DrawerContent className="bg-black">
+    <div className="w-full h-auto p-8 flex">
+      <TextGenerateEffect words={tagline}/>
+    </div>
+  </DrawerContent>
+</Drawer>
+</div>
+            </div>
+          </div>
+        </p>
+      </div>
+    </main>
+  );
+}
---
+++ components/Navbar.tsx
@@ -0,0 +1,114 @@
+import Image from "next/image";
+import { Button } from "./ui/button";
+import {
+  Drawer,
+  DrawerClose,
+  DrawerContent,
+  DrawerDescription,
+  DrawerFooter,
+  DrawerHeader,
+  DrawerTitle,
+  DrawerTrigger,
+} from "@/components/ui/drawer";
+
+import {
+    CalendarIcon,
+    EnvelopeClosedIcon,
+    FaceIcon,
+    GearIcon,
+    PersonIcon,
+    RocketIcon,
+  } from "@radix-ui/react-icons"
+   
+  import {
+    Command,
+    CommandEmpty,
+    CommandGroup,
+    CommandInput,
+    CommandItem,
+    CommandList,
+    CommandSeparator,
+    CommandShortcut,
+  } from "@/components/ui/command"
+
+  import {
+    Sheet,
+    SheetContent,
+    SheetDescription,
+    SheetHeader,
+    SheetTitle,
+    SheetTrigger,
+  } from "@/components/ui/sheet"
+
+export default function NavBar() {
+  return (
+    <nav className="flex h-24 w-full bg-black">
+      <div className=" h-full w-1/5 p-6"></div>
+      <div className="h-full w-3/5 p-6 flex items-center justify-center">
+        <Drawer>
+          <DrawerTrigger>
+            {" "}
+            <button className="shadow-[0_0_0_3px_#000000_inset] pl-2 pr-32 w-full py-1 bg-transparent border text-sm text-white/80 border-black dark:border-white dark:text-white/70 text-black rounded-lg transform  transition duration-400">
+              Search...
+            </button>
+          </DrawerTrigger>
+          <DrawerContent className="bg-black">
+            <div className="w-full h-auto p-8 flex items-center justify-center">
+            <Command className="rounded-lg bg-black">
+      <CommandInput placeholder="Search or navigate pages..." />
+      <CommandList>
+        <CommandEmpty>No results found.</CommandEmpty>
+        
+        <CommandSeparator />
+        <CommandGroup heading="Settings">
+          <CommandItem>
+            <PersonIcon className="mr-2 h-4 w-4" />
+            <span>Account</span>
+            <CommandShortcut>⌘A</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <EnvelopeClosedIcon className="mr-2 h-4 w-4" />
+            <span>Notifications</span>
+            <CommandShortcut>⌘N</CommandShortcut>
+          </CommandItem>
+          <CommandItem>
+            <GearIcon className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            <CommandShortcut>⌘S</CommandShortcut>
+          </CommandItem>
+        </CommandGroup>
+      </CommandList>
+    </Command>
+            </div>
+          </DrawerContent>
+        </Drawer>
+      </div>
+      <div className=" h-full w-1/5 p-6  flex items-center justify-center">
+
+<Sheet>
+  <SheetTrigger><button><svg width="24" height="24" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">
+<path d="M5 9H13H19M5 15H19" stroke="white" stroke-width="2" stroke-linecap="round"/>
+</svg>
+</button></SheetTrigger>
+  <SheetContent side={'left'} className="bg-black">
+    <div className="h-full w-full py-24">
+            
+            <div className="flex items-center p-4"><PersonIcon className="mr-2 h-4 w-4" />
+            <span>Account</span>
+            </div>
+            <div className="flex items-center p-4">
+            <EnvelopeClosedIcon className="mr-2 h-4 w-4" />
+            <span>Notifications</span>
+            </div>
+            <div className="flex items-center p-4">
+            <GearIcon className="mr-2 h-4 w-4" />
+            <span>Settings</span>
+            </div>
+    </div>
+  </SheetContent>
+</Sheet>
+
+      </div>
+    </nav>
+  );
+}

fe31a64e194463205f441d9cb6275a2f743b2ae6
deepabhyudaya <154480782+deepabhyudaya@users.noreply.github.com>
2024-02-28 18:29:58 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -177,7 +177,7 @@ export default function Home() {
           title={item.title}
           description={item.description}
           header={item.header}
-          icon={item.icon}
+      
           className={i === 3 || i === 6 ? "md:col-span-2" : ""}
         />
       ))}
